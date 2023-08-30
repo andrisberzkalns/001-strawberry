5c62038c96d94c2649352466ed73638ee91444be
Andris <berzkalns.andris@gmail.com>
2023-08-31 01:40:32 +0300

    fixing paths

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,17 @@ import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 import path from 'path';
 import { normalizePath } from 'vite'
+import tailwindcss from 'tailwindcss'
 
 export default defineConfig({
   root: 'src',
   build: {
     outDir: '../dist'
   },
+  content: [
+    "./src/index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   plugins: [
     viteStaticCopy({
       targets: [
